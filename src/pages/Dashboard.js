bfb56598ec9107b76d0cc182939f2f0abcfb8bf2
Lodenn <khalednasser788@gmail.com>
2021-08-07 21:23:24 +0200

    add starter project files

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
+const Dashboard = () => {
+  return (
+    <main>
+      <h2>Dashboard Page</h2>
+    </main>
+  );
+};
+
+export default Dashboard;
