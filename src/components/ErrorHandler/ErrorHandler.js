0cb5558251ad11081ac7f666a081cb9ce81e98ca
Lodenn <khalednasser788@gmail.com>
2021-08-08 03:58:17 +0200

    fix bug

---
+++ src/components/ErrorHandler/ErrorHandler.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  align-content: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`;
+
+const ErrorHandler = () => {
+  return (
+    <Wrapper>
+      <h1>404</h1>
+      <h3>Sorry, this page you are looking for is not found</h3>
+      <Link to="/" className="btn">
+        Back home
+      </Link>
+    </Wrapper>
+  );
+};
+
+export default ErrorHandler;
